a7a463d9626e70ef70a90010b5b09c9cd00debdd
july <fanmengqiu@126.com>
2017-03-11 13:39:05 +0000

    add case:test pluse 2

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -7,8 +7,11 @@ import calculator.calculator
 
 
 class TestCalculator:
-    def test_pluse(self):
+    def test_pluse_1(self):
         assert 5 == calculator.calculator.plus(1, 4)
 
     def test_minus(self):
         assert 2 == calculator.calculator.minus(5, 3)
+
+    def test_pluse_2(self):
+        assert 3!= calculator.calculator.plus(1, 1)
